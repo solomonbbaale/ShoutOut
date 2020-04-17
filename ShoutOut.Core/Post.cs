67921d605eefc16baf317dabcd6fa49b44b86055
Solomon Bbaale <shbbaale03@yahoo.co.uk>
2020-04-17 14:25:54 +0100

    added initial models

---
+++ ShoutOut.Core/Post.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace ShoutOut.Core
+{
+    public class Post : Entity
+    {
+        public Post(string title, string message, User owner, int posterOwnerId, User postOwner)
+        {
+            Title = title;
+            Message = message;
+            PosterOwnerId = posterOwnerId;
+            PostOwner = postOwner;
+            Replies = new List<Post>();
+        }
+
+        public string Title { get; set; }
+        public string Message { get; set; }
+        public IEnumerable<Post> Replies { get; }
+        public User PostOwner { get; set; }
+        public int PosterOwnerId { get; set; }
+    }
+}
---
+++ ShoutOut.Core/User.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace ShoutOut.Core
+{
+    public class User : Entity
+    {
+        public User(string handle, string firstName, string lastName, string bio, string email, string profile)
+        {
+            Handle = handle;
+            FirstName = firstName;
+            LastName = lastName;
+            Bio = bio;
+            Email = email;
+            Profile = profile;
+            Posts = new List<Post>();
+        }
+
+        public IEnumerable<Post> Posts { get; }
+        public string Handle { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Bio { get; set; }
+        public string Email { get; set; }
+        public string Profile { get; set; }
+    }
+}
