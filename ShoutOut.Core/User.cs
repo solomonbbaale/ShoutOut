4cd77aa537e9c7dafdbfe32e5c836e6c2eb03504
Solomon Bbaale <shbbaale03@yahoo.co.uk>
2020-04-17 14:39:40 +0100

    added images to core

--- ShoutOut.Core/User.cs
+++ ShoutOut.Core/User.cs
@@ -2,6 +2,20 @@
 
 namespace ShoutOut.Core
 {
+    public class ShoutImage : Entity
+    {
+        public ShoutImage(string name, string extension, byte[] imageBytes)
+        {
+            Name = name;
+            Extension = extension;
+            ImageBytes = imageBytes;
+        }
+
+        public string Name { get; set; }
+        public string Extension { get; set; }
+        public byte[] ImageBytes { get; set; }
+    }
+
     public class User : Entity
     {
         public User(string handle, string firstName, string lastName, string bio, string email, string profile)
