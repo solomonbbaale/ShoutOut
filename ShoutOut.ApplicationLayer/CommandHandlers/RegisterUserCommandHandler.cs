5da924525333b4f30ad5fc32861fce13f0064a52
Solomon Bbaale <shbbaale03@yahoo.co.uk>
2020-04-17 21:03:30 +0100

    Adde application service layer Added Initial Commands and handlers

---
+++ ShoutOut.ApplicationLayer/CommandHandlers/RegisterUserCommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using ShoutOut.ApplicationLayer.Helpers;
+using ShoutOut.Core.Commands;
+
+namespace ShoutOut.ApplicationLayer.CommandHandlers
+{
+    public class RegisterUserCommandHandler:ICommandHandler<RegisterUserCommand>
+    {
+        public RegisterUserCommandHandler(RegisterUserCommand command)
+        {
+
+        }
+        public Result Execute(RegisterUserCommand command)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
